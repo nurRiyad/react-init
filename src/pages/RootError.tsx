ac47bc0374b6848d23364a9d7fcb2dfc41e30f6e
nurRiyad <asadnurriyad@gmail.com>
2024-03-04 23:20:07 +0600

    Update error page
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/pages/RootError.tsx
+++ src/pages/RootError.tsx
@@ -1,5 +1,28 @@
-function RootError() {
-  return <div>Something Went Wrong</div>;
-}
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 
-export default RootError;
+export default function ErrorPage() {
+  const error = useRouteError();
+
+  const errMsg = errorMessage(error);
+
+  function errorMessage(error: unknown): string {
+    if (isRouteErrorResponse(error)) {
+      return `${error.status} ${error.statusText}`;
+    } else if (error instanceof Error) {
+      return error.message;
+    } else if (typeof error === 'string') {
+      return error;
+    } else {
+      console.error(error);
+      return 'Unknown error';
+    }
+  }
+
+  return (
+    <div id="error-page">
+      <p>
+        <i>{errMsg}</i>
+      </p>
+    </div>
+  );
+}
